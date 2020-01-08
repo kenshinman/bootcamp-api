dd9cc673f8fff724a1ee4aa0e5047b6770645203
Kehinde Orilogbon <archkenny@yahoo.com>
2020-01-08 13:27:05 +0100

    imported r esources

--- config/db.js
+++ config/db.js
@@ -4,7 +4,8 @@ const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
-      useUnifiedTopology: true
+      useUnifiedTopology: true, useCreateIndex: true,
+      useFindAndModify: false
     })
 
     console.log(`MongoDB Connected: ${conn.connection.host}`)
--- server.js
+++ server.js
@@ -20,4 +20,12 @@ app.use("/api/v1/bootcamps", bootcamps)
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
+const server = app.listen(PORT, () => console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
+
+//hanlde unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`);
+
+  server.close(() => process.exit(1))
+
+})
