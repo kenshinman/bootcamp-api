f99f508d04d3f4e3721a693507a3201b7232cce5
Kehinde Orilogbon <archkenny@yahoo.com>
2020-01-08 13:16:23 +0100

    connected db

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
-const express = require("express")
+const express = require("express");
 const dotenv = require("dotenv");
-
+const morgan = require("morgan");
+const connectDB = require("./config/db")
 //Route  files
 const bootcamps = require("./routes/bootcamps")
 
 //Load env vars
 dotenv.config({ path: "./config/config.env" })
 
+connectDB();
+
 const app = express();
 
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"))
+}
 //mount routes
 app.use("/api/v1/bootcamps", bootcamps)
 
