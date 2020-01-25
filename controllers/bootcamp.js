dd0c986a05eee10fe76ba38b303eddb9c72a02eb
Kehinde Orilogbon <archkenny@yahoo.com>
2020-01-25 06:42:17 +0100

    - added User import to seeder, - Added User to Bootcamp     schema - added User  to the  bootcamp    creation payload

--- controllers/bootcamp.js
+++ controllers/bootcamp.js
@@ -28,6 +28,22 @@ exports.getBootcamp = asyncHandler(async (req, res, next) => {
 //@route POST /api/v1/bootcamps
 //@access private
 exports.createBootcamp = asyncHandler(async (req, res, next) => {
+	//add req.user to req.body
+	req.body.user = req.user.id;
+
+	//check for publidhed  user
+	const publishedBootcamp = await Bootcamp.findOne({user: req.user.id});
+
+	//if  user is not an admin, he can't add  more than one bootcamp
+	if (publishedBootcamp && req.user.role !== "admin") {
+		return next(
+			new ErrorResponse(
+				`The user with ID ${req.user.id} has already publidhed a bootcamp`,
+				403
+			)
+		);
+	}
+
 	const bootcamp = await Bootcamp.create(req.body);
 	res.status(201).json({
 		success: true,
--- seeder.js
+++ seeder.js
@@ -9,13 +9,14 @@ dotenv.config({path: "./config/config.env"});
 //Load models
 const Bootcamp = require("./models/Bootcamp");
 const Course = require("./models/Course");
+const User = require("./models/User");
 
 //connect db
 mongoose.connect(process.env.MONGO_URI, {
 	useNewUrlParser: true,
 	useUnifiedTopology: true,
 	useCreateIndex: true,
-	useFindAndModify: false,
+	useFindAndModify: false
 });
 
 //read file
@@ -25,6 +26,9 @@ const bootcamps = JSON.parse(
 const courses = JSON.parse(
 	fs.readFileSync(`${__dirname}/_data/courses.json`, "utf-8")
 );
+const users = JSON.parse(
+	fs.readFileSync(`${__dirname}/_data/users.json`, "utf-8")
+);
 
 //import into db
 
@@ -32,6 +36,7 @@ const importData = async () => {
 	try {
 		await Bootcamp.create(bootcamps);
 		await Course.create(courses);
+		await User.create(users);
 		console.log("Data imported".green.inverse);
 		process.exit(1);
 	} catch (error) {
@@ -45,6 +50,7 @@ const deleteData = async () => {
 	try {
 		await Bootcamp.deleteMany();
 		await Course.deleteMany();
+		await User.deleteMany();
 		console.log("Data destroyed".red.inverse);
 		process.exit(1);
 	} catch (error) {
